85a48e764632ce9c170d1ae35bc232e7443d5ef4
gyl <gaoyongliang@ygsoft.com>
2018-06-20 22:52:36 +0800

    perl-mysql-demo-1.pl ... add

---
+++ perl-2018-training/perl-mysql-demo-1.pl/perl-mysql-demo-1.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+# perl-mysql-demo-1.pl
+
+use strict;
+use DBI;
+
+my $host     = 'localhost';
+my $driver   = 'mysql';
+my $database = 'mysql';
+
+my $dsn = "DBI:$driver:database=$database:$host";
+print $dsn;
+
+my $username = 'root';
+my $password = '';
+
+my $dbh = DBI->connect($dsn, $username, $password) or die $DBI::errstr;
+my $sth = $dbh->prepare('show databses');
+$sth->execute();
+
+while(my @row = $sth->fetchrow_array()){
+	print join('\t', @row) . "\n";
+}
+$sth->finish();
+$dbh->disconnect();
