d6f90597ac2d003f9e4a5149dc0abfbd8308336d
gyl <gaoyongliang@ygsoft.com>
2018-06-20 21:52:35 +0800

    perl-additive-hash-demo-1.pl ... modify

--- perl-Arithmetic-2018/perl-Arithmetic-2018-hash/perl-additive-hash-demo-1.pl/perl-additive-hash-demo-1.pl
+++ perl-Arithmetic-2018/perl-Arithmetic-2018-hash/perl-additive-hash-demo-1.pl/perl-additive-hash-demo-1.pl
@@ -10,18 +10,29 @@ sub Main{
 	for(my $i = 0; $i < $n; $i++){
 		printf("%-10d %-15s %3d\n", $i, $A_strKeys[$i], AdditiveHash($A_strKeys[$i], 31));
 	}
-	print 'Done.'."\n";
 }
 
 sub AdditiveHash{
 	my ($s, $nPrime) = @_;
-	my $n = length($s), $nHash = $n;
-
-	# print $s. ' ' . $n . ' ' . $nPrime . "\n";
+	my $n = length($s);
+	my $nHash = $n;
 	for(my $i = 0; $i < $n; $i++){
-		my $ch = substr($s, $i, 1);
-		# print $ch . ' ' . ord($ch) . "\n";
-		$nHash += ord($ch);
+		$nHash += ord(substr($s, $i, 1));
 	}
 	$nHash % $nPrime;
 }
+
+=result
+0          C                 6
+1          C++               1
+2          Java             18
+3          C#               11
+4          Python           28
+5          Go               29
+6          Scala            24
+7          vb.net            6
+8          JavaScript        2
+9          PHP              18
+10         Perl              4
+11         Ruby             19
+=cut
